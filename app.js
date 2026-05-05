a1c44d21a523c332cdb0a17974b288b2e97ef9af
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-05-05 09:21:53 +0400

    masivi

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const numbers=[100,500,400,5000,20];
 
 const avaerage=numbers.length;
 const sum=numbers;
+const sum=0;
+for(let i=0;i<numbers.length;i++){
+    sum+=numbers[i];
+}
+const average=sum/numbers.length;
 
 console.log(sum);
-console.log(avaerage);
+console.log(average);
